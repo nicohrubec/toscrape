a743d4c201fc5df8a846898060b21fe4c6ae308c
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2023-08-30 16:26:16 +0200

    Put book scraper into separate class

--- src/main.py
+++ src/main.py
@@ -1,65 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-from tqdm import tqdm
+from src.scrapers.BooksScraper import BooksScraper
 
 URL = 'http://books.toscrape.com'
 
 
 def main():
-    book_uris = []
-    book_data = []
-
-    # get book urls
-    print("Retrieve URLs ...")
-    for i in tqdm(range(1, 2)): ## change to while True and catch 404s and other errors
-        page_url = URL + f'/catalogue/page-{i}.html'
-        response = requests.get(page_url)
-
-        if response.status_code == 404:
-            break
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        books = soup.findAll('article', {'class': 'product_pod'})
-
-        for book in books:
-            book_uri = book.find('a')
-            book_uris.append(book_uri['href'])
-
-    # scrape individual pages
-    print("Scrape book pages ...")
-    for book_uri in tqdm(book_uris):
-        book_url = URL + '/catalogue/' + book_uri
-        response = requests.get(book_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        book_info = soup.find('div', {'class': 'product_main'})
-
-        title = book_info.find('h1').text.strip()
-        price = book_info.find('p', {'class': 'price_color'}).text.strip()[1:]
-        availability = book_info.find('p', {'class': 'instock availability'}).text.strip()
-
-        info_table = soup.find('table')
-
-        upc = '-'
-        for row in info_table.findAll('tr'):
-            if row.find('th').text.strip() == 'UPC':
-                upc = row.find('td').text
-
-        description = soup.findAll('p')[-1].text
-
-        book_data.append({
-            'title': title,
-            'price': price,
-            'availability': availability,
-            'upc': upc,
-            'description': description
-        })
-
-        break
-
-    book_data = pd.DataFrame(book_data)
-    print(book_data.head())
-
+    bs = BooksScraper()
+    data = bs.scrape()
+    print(data.head())
 
 if __name__ == '__main__':
     main()
---
+++ src/scrapers/BooksScraper.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+
+
+class BooksScraper:
+    URL = 'http://books.toscrape.com'
+
+    def _get_uris(self):
+        book_uris = []
+
+        print("Retrieve URLs ...")
+        for i in tqdm(range(1, 3)):
+            page_url = self.URL + f'/catalogue/page-{i}.html'
+            response = requests.get(page_url)
+
+            if response.status_code == 404:
+                break
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+            books = soup.findAll('article', {'class': 'product_pod'})
+
+            for book in books:
+                book_uri = book.find('a')
+                book_uris.append(book_uri['href'])
+
+        return book_uris
+
+    def _scrape_uri(self, uri):
+        book_url = self.URL + '/catalogue/' + uri
+        response = requests.get(book_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        book_info = soup.find('div', {'class': 'product_main'})
+
+        title = book_info.find('h1').text.strip()
+        price = book_info.find('p', {'class': 'price_color'}).text.strip()[1:]
+        availability = book_info.find('p', {'class': 'instock availability'}).text.strip()
+
+        info_table = soup.find('table')
+
+        upc = '-'
+        for row in info_table.findAll('tr'):
+            if row.find('th').text.strip() == 'UPC':
+                upc = row.find('td').text
+
+        description = soup.findAll('p')[-1].text
+
+        return {
+            'title': title,
+            'price': price,
+            'availability': availability,
+            'upc': upc,
+            'description': description
+        }
+
+    def scrape(self):
+        book_data = []
+        book_uris = self._get_uris()
+
+        for book_uri in tqdm(book_uris):
+            book_entry = self._scrape_uri(book_uri)
+            book_data.append(book_entry)
+
+        book_data = pd.DataFrame(book_data)
+        return book_data
